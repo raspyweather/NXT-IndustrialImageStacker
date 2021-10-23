f82e6b9e0b8d557ee2d647ecfd4a1d0ae8d5cd52
raspyweather <raspyweather@gmail.com>
2021-10-23 16:35:56 +0200

    feat: add min filter

--- ImageFilters/imagefilters.h
+++ ImageFilters/imagefilters.h
@@ -6,6 +6,7 @@
 class ImageFilters
 {
     static void max(const uint8_t* inputImageA,const uint8_t* inputImageB, uint8_t* output, size_t size);
+    static void min(const uint8_t* inputImageA,const uint8_t* inputImageB, uint8_t* output, size_t size);
 private:
     ImageFilters();
 };
